6b0fcce9cec32e2b2b2d05eff63bb6e60c86836f
Alistair Bahr <alistairbahr@outlook.com>
2022-03-13 13:23:38 +0100

    Elden Ring Fextralife fullscreen map redirect added

--- elden-ring-fextralife-map-redirect.user.js
+++ elden-ring-fextralife-map-redirect.user.js
@@ -1,1 +1,20 @@
-‎‎​
+// ==UserScript==
+// @name         Elden Ring Fextralife fullscreen map redirect
+// @namespace    https://greasyfork.org/en/users/12725-alistair1231
+// @version      0.1
+// @description  redirects to fullscreen map when opening a map link
+// @author       Alistair1231
+// @match        https://eldenring.wiki.fextralife.com/Interactive+Map*
+// @icon         https://icons.duckduckgo.com/ip2/fextralife.com.ico
+// @grant        none
+// @downloadURL  https://gist.github.com/Auncaughbove17/1efc6138988425c938e6289736ada85d/raw/elden-ring-fextralife-map-redirect.user.js
+// @license GPL-3.0
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var map = Array.from(document.getElementsByTagName("iframe")).filter(x => x.classList.contains("interactivemapcontainer") && x.parentElement.parentElement.style.display!="none")[0];
+    document.location=map.src;
+})();
+
+

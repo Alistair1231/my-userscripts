a6e6b0dc7b6ee04b88419b784ea1dfe896538671
Alistair Bahr <alistairbahr@outlook.com>
2022-03-16 10:24:20 +0100

    map

--- elden-ring-fextralife-map-redirect.user.js
+++ elden-ring-fextralife-map-redirect.user.js
@@ -5,6 +5,9 @@
 // @description  redirects to fullscreen map when opening a map link
 // @author       Alistair1231
 // @match        https://eldenring.wiki.fextralife.com/Interactive+Ma*
+// @match        https://eldenring.wiki.fextralife.com/Interactive+ma*
+// @match        https://eldenring.wiki.fextralife.com/interactive+ma*
+// @match        https://eldenring.wiki.fextralife.com/interactive+Ma*
 // @icon         https://icons.duckduckgo.com/ip2/fextralife.com.ico
 // @grant        none
 // @downloadURL  https://gist.github.com/Auncaughbove17/1efc6138988425c938e6289736ada85d/raw/elden-ring-fextralife-map-redirect.user.js

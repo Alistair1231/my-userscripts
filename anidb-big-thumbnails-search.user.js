dc05648253c1e92162cf52ac8a21d94a71f736c4
Alistair Bahr <alistairbahr@outlook.com>
2022-04-02 23:13:37 +0200

    anidb search

---
+++ anidb-big-thumbnails-search.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         AniDB Big Thumbnails on Search
+// @namespace    https://greasyfork.org/en/users/12725-alistair1231
+// @version      0.1.1
+// @description  Adds big thumbnails to the search results on AniDB
+// @author       Alistair1231
+// @match        https://myanimelist.net/anime/*
+// @icon         https://icons.duckduckgo.com/ip2/anidb.net.ico
+// @grant       none
+// @require https://code.jquery.com/jquery-3.6.0.min.js
+// @downloadURL  https://github.com/Auncaughbove17/my-userscripts/raw/main/anidb-big-thumbnails-search.user.js
+// @license GPL-3.0
+// ==/UserScript==
+
+function addStyleAttribute($element, styleAttribute) {
+    $element.attr('style', $element.attr('style') + '; ' + styleAttribute);
+}
+
+    var thumbnailContext = $($('.thumb.anime a picture'))
+    var allThumbs = $("source,img", thumbnailContext)
+
+    for (i = 0; i < allThumbs.length ;i++) {
+        if (allThumbs[i].flag != 1) {
+            var bigImg = allThumbs[i].srcset;
+            allThumbs[i].flag = 1;
+            allThumbs[++i].src = bigImg;
+            allThumbs[i].width *= 3;
+            allThumbs[i].height *= 3;
+            allThumbs[i].loading = "eager";
+            addStyleAttribute($(allThumbs[i]), 'max-width: 18.5em !important');
+        }
+    }
+

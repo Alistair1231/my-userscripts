3b12d4308b0ba26538d3b9eff62c715388ace588
Auncaughbove17 <alistairbahr@outlook.com>
2022-06-25 13:29:32 +0200

    Update hanime-1080p.user.js

--- hanime-1080p.user.js
+++ hanime-1080p.user.js
@@ -1,5 +1,5 @@
 // open settings
-$("button.vjs-control:nth-child(9)").click() 
+$("html.vveuuwfs.idc0_341 body div#omni_player div#primary_video.htv_video.video-js.vjs-default-skin.vjs-has-started.vjs-controls-enabled.vjs-workinghover.vjs-v7.primary_video-dimensions.vjs-fluid.vjs-paused.vjs-user-inactive div.vjs-control-bar button.vjs-control.vjs-button.vjs-icon-cog").click() 
 // hide selector and overlay
 $(".overlay").hide() 
 $(".htv-server-selector-dialog").hide() 

7baeae616fa926d286d41b58fdd274972fc1f002
Auncaughbove17 <alistairbahr@outlook.com>
2022-06-25 13:26:17 +0200

    Update hanime-1080p.user.js

--- hanime-1080p.user.js
+++ hanime-1080p.user.js
@@ -4,4 +4,5 @@ $("button.vjs-control:nth-child(9)").click()
 $(".overlay").hide() 
 $(".htv-server-selector-dialog").hide() 
 // click 1080p
-$("div.align-center:nth-child(3)").click()
+setTimeout($("div.align-center:nth-child(3)").click(),30)
+

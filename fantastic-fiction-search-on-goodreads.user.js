09feabf88474d598e4006403ca5c1570c53ca2f0
Auncaughbove17 <alistairbahr@outlook.com>
2022-07-21 20:09:35 +0200

    Update fantastic-fiction-search-on-goodreads.user.js

--- fantastic-fiction-search-on-goodreads.user.js
+++ fantastic-fiction-search-on-goodreads.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         fantastic fiction search on goodreads
 // @namespace    https://greasyfork.org/en/users/12725-alistair1231
-// @version      0.1.1
+// @version      0.1
 // @description  adds button to fantastic fiction for searching on goodreads
 // @author       Alistair1231
 // @match        https://www.fantasticfiction.com/k/*
@@ -12,16 +12,6 @@
 // @license GPL-3.0
 // ==/UserScript==
 
-
-(function() {
-    'use strict';
-    let j = jQuery;
-
-    var author = j(".ff span[itemprop='author']").children(0).children()[0].outerText;
-    var title= j(".bookheading").children()[0].innerText;
-    var link = `https://www.goodreads.com/search?q=${title}+${author}`;
-    var entry = createEntry(title,link);
-})();
 
 function createEntry(text,link){
     var span = document.createElement("span");
@@ -36,3 +26,14 @@ function createEntry(text,link){
     `;
     return span;
 }
+
+(function() {
+    'use strict';
+    let j = jQuery;
+
+    var author = j(".ff span[itemprop='author']").children(0).children()[0].outerText;
+    var title= j(".bookheading").children()[0].innerText;
+    var link = `https://www.goodreads.com/search?q=${title}+${author}`;
+    var entry = createEntry(title,link);
+    j(".book-shop").children(0).after(entry);
+})();

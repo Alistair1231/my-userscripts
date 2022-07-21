20b138cb7d9346e11e77ee0a8d5f9ff91ef4e85d
Auncaughbove17 <alistairbahr@outlook.com>
2022-07-21 19:56:49 +0200

    Update fantastic-fiction-search-on-goodreads.user.js

---
+++ fantastic-fiction-search-on-goodreads.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         
+// @namespace    https://greasyfork.org/en/users/12725-alistair1231
+// @version      0.1.1
+// @description  auto selects never expire when making a paste
+// @author       Alistair1231
+// @match        https://www.fantasticfiction.com/k/dante-king/aether-mage-1.htm
+// @icon         https://icons.duckduckgo.com/ip2/fantasticfiction.com.ico
+// @grant        none
+// @downloadURL  https://github.com/Auncaughbove17/my-userscripts/raw/main/0binExpire.user.js
+// @license GPL-3.0
+// ==/UserScript==
+
+
+jQuery(".ff span[itemprop='author']").children(0).children()[0].outerText

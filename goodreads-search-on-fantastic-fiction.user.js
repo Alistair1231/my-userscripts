ab8a5599cf8d1ae4804c13436c2e08307c9bfd27
Auncaughbove17 <alistairbahr@outlook.com>
2022-07-21 20:20:23 +0200

    Update goodreads-search-on-fantastic-fiction.user.js

--- goodreads-search-on-fantastic-fiction.user.js
+++ goodreads-search-on-fantastic-fiction.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  adds button to goodreads for searching on fantastic fiction 
 // @author       Alistair1231
-// @match        https://www.goodreads.com/book/show/59009472-aether-mage?from_search=true&from_srp=true&qid=pCZXDpccdA&rank=1
+// @match        https://www.goodreads.com/book/show/*
 // @icon         https://icons.duckduckgo.com/ip2/goodreads.com.ico
 // @grant        none
 // @license GPL-3.0

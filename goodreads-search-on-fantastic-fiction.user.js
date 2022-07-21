66d590b58a56c85863dce4ed1bca18bea9b69429
Auncaughbove17 <alistairbahr@outlook.com>
2022-07-21 20:20:17 +0200

    Update goodreads-search-on-fantastic-fiction.user.js

--- goodreads-search-on-fantastic-fiction.user.js
+++ goodreads-search-on-fantastic-fiction.user.js
@@ -2,7 +2,7 @@
 // @name         goodreads search on fantastic fiction
 // @namespace    https://greasyfork.org/en/users/12725-alistair1231
 // @version      0.1
-// @description  auto selects never expire when making a paste
+// @description  adds button to goodreads for searching on fantastic fiction 
 // @author       Alistair1231
 // @match        https://www.goodreads.com/book/show/59009472-aether-mage?from_search=true&from_srp=true&qid=pCZXDpccdA&rank=1
 // @icon         https://icons.duckduckgo.com/ip2/goodreads.com.ico

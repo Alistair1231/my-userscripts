5358077f56463a3196421e151f86ba7a31eec71b
Auncaughbove17 <alistairbahr@outlook.com>
2022-07-21 20:22:17 +0200

    Update goodreads-search-on-fantastic-fiction.user.js

--- goodreads-search-on-fantastic-fiction.user.js
+++ goodreads-search-on-fantastic-fiction.user.js
@@ -26,8 +26,20 @@ function createEntry(text,link){
     `;
     return span;
 }
+// Add 'Search MAM' button
+var buttonBar = document.getElementById("buyButtonContainer");
+if (buttonBar === null || buttonBar == "null") {
+    buttonBar = document.getElementById("asyncBuyButtonContainer");
+}
+var buttonUl  = buttonBar.getElementsByTagName("ul");
+var mamButton = document.createElement("li");
+mamButton.innerHTML = '<a id="mamLink" href="' + mamSearchUrl + '" target="_blank" class="buttonBar">Search MAM</a>';
+mamButton.className = "Button";
+buttonUl[0].appendChild(mamButton);
+console.log("[G+] 'Search MAM' button added!");
 
 (function() {
     'use strict';
 
+    
 })();

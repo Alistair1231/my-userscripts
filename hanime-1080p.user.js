370f238e820028e9e199ea2ab0298b084a595ca9
Alistair Bahr <alistairbahr@outlook.com>
2022-07-29 00:53:47 +0200

    chore: delete hanime-1080p.user.js

--- hanime-1080p.user.js
+++
@@ -1,8 +0,0 @@
-// open settings
-$("html.vveuuwfs.idc0_341 body div#omni_player div#primary_video.htv_video.video-js.vjs-default-skin.vjs-has-started.vjs-controls-enabled.vjs-workinghover.vjs-v7.primary_video-dimensions.vjs-fluid.vjs-paused.vjs-user-inactive div.vjs-control-bar button.vjs-control.vjs-button.vjs-icon-cog").click() 
-// hide selector and overlay
-$(".overlay").hide() 
-$(".htv-server-selector-dialog").hide() 
-// click 1080p
-setTimeout(()=>{$("div.align-center:nth-child(3)").click()},30)
-

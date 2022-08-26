0ba9eb416bb0db6c5054f8d73b3f38ca159464a0
Alistair Bahr <alistairbahr@outlook.com>
2022-08-26 22:25:15 +0200

    update MAM-folder-path.user.js

--- MAM-folder-path.user.js
+++ MAM-folder-path.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MAM folder path
 // @namespace    https://greasyfork.org/en/users/12725-alistair1231
-// @version      0.3.1
+// @version      0.4
 // @description  Add Audiobook folder path to torrent info
 // @author       Alistair1231
 // @include      https://www.myanonamouse.net/t/*
@@ -9,71 +9,87 @@
 // @grant        none
 // ==/UserScript==
 
-function decodeHtml(html) {
+
+/////////////////////////
+// find book info and modify it
+
+const decodeHtml = (html) => {
     // make sure there is no &amp; or similiar in the string
     var txt = document.createElement("textarea");
     txt.innerHTML = html;
     return txt.value;
 }
-
-function replaceSymbols(val){
-    return val.replaceAll(':',' -').replaceAll('&','and').replaceAll(',','_');
-}
+const replaceSymbols = (val) => val.replaceAll(':', ' -').replaceAll('&', 'and').replaceAll(',', '_');
 
 var bookTitle = replaceSymbols(decodeHtml(document.getElementsByClassName("TorrentTitle")[0].innerHTML)).trim();
 var author = replaceSymbols(decodeHtml(document.getElementsByClassName("torDetRight torAuthors")[0].textContent)).trim();
 var series = "";
-var bookOfSeries= "";
+var bookOfSeries = "";
 
+// if part of a series, add the series name and the book number to the folder path
+
+const addLeadingZero = (bookOfSeries) => {
+    // since you cannot add leading zeros without splitting the float into two integers, we have to do this
+    let array = String(bookOfSeries).split('.')
+    array[0] = array[0].padStart(2, '0')
+    return array.join('.')
+}
 
 try {
     series = replaceSymbols(decodeHtml(document.getElementsByClassName("torDetRight torSeries")[0].firstChild.firstChild.text)).trim();
-    // extract number of book including subpart like 3.1 or 3.5
-    bookOfSeries = String(document.getElementsByClassName("torDetRight torSeries")[0].firstChild.childNodes[1].data.match(/\d+\.?\d*/)).padStart(2, '0');
-    // since you cannot add leading zeros without splitting the float into two integers, we have to do this
-    let array = bookOfSeries.toString().split('.')
-    array[0] = array[0].padStart(2, '0')
-    bookOfSeries = array.join('.')
-} catch (TypeError) {
-}
-    
-if(series != "") {
-var folderPath = `/_Audiobooks/${author} - ${series}/Book ${bookOfSeries} - ${bookTitle}`
-var folderPath2 = `/_Audiobooks1/${author} - ${series}/Book ${bookOfSeries} - ${bookTitle}`
+    // extract number of book including subpart like 3.1 or 3.5 and add leading zero if needed
+    bookOfSeries = addLeadingZero(document.getElementsByClassName("torDetRight torSeries")[0].firstChild.childNodes[1].data.match(/\d+\.?\d*/)).padStart(2, '0');}
+catch (TypeError) { }
+
+if (series != "") {
+    var folderPath = `/_Audiobooks/${author} - ${series}/Book ${bookOfSeries} - ${bookTitle}`;
+    var folderPath2 = `/_Audiobooks1/${author} - ${series}/Book ${bookOfSeries} - ${bookTitle}`;
+    var folderPath3 = `Book ${bookOfSeries} - ${bookTitle}`;
 } else {
-var folderPath = `/_Audiobooks/${author} - Loose Books/${bookTitle}`
-var folderPath2 = `/_Audiobooks1/${author} - Loose Books/${bookTitle}`
+    var folderPath = `/_Audiobooks/${author} - Loose Books/${bookTitle}`;
+    var folderPath2 = `/_Audiobooks1/${author} - Loose Books/${bookTitle}`;
+    var folderPath3 = `${bookTitle}`;
 }
 
-var narratorDiv  = document.getElementById("Narrator").parentElement;
+var narratorDiv = document.getElementById("Narrator").parentElement;
 var folderText = document.createElement("div");
+
+
+/////////////////////////
+// create HTML to inject and inject it
+
+const createFolderText = (idA, idP, folderPath) => `
+    <div id="Folder" class="torDetRight torSeries">
+            <span class="flex"><a id='${idA}'>${folderPath}</a>
+            <p id='${idP}' style="font-size: 9px;margin-left: 10px;"></p></span>
+    </div>
+    `;
+
 folderText.innerHTML = `
 <div class="torDetRow">
     <div class="torDetLeft">Folder Path</div>
-    <div id="Folder" class="torDetRight torSeries">
-        <span class="flex"><a id='folderPath'>${folderPath}</a>
-        <p id='textCopied' style="font-size: 9px;margin-left: 10px;"></p></span>
-    </div>
-    <div id="Folder2" class="torDetRight torSeries">
-        <span class="flex"><a id='folderPath2'>${folderPath2}</a>
-        <p id='textCopied2' style="font-size: 9px;margin-left: 10px;"></p></span>
-    </div>
+    ${createFolderText("folderPath", "textCopied", folderPath)}
+    ${createFolderText("folderPath2", "textCopied2", folderPath2)}
+    ${createFolderText("folderPath3", "textCopied3", folderPath3)}
 </div>
 `
 narratorDiv.before(folderText);
 
+/////////////////////////
 // make click to copy
-document.getElementById("folderPath").addEventListener("click", function() {
-    navigator.clipboard.writeText(folderPath);
-    document.getElementById("textCopied").innerHTML = "Copied!";
-        setTimeout(function() {
-            document.getElementById("textCopied").innerHTML = '';
+
+const createClickToCopy = (idA, idP, value) => {
+    document.getElementById(idA).addEventListener("click", function () {
+        navigator.clipboard.writeText(value);
+        document.getElementById(idP).innerHTML = "Copied!";
+        setTimeout(function () {
+            document.getElementById(idP).innerHTML = '';
         }, 1000);
-  });
-document.getElementById("folderPath2").addEventListener("click", function() {
-    navigator.clipboard.writeText(folderPath);
-    document.getElementById("textCopied2").innerHTML = "Copied!";
-        setTimeout(function() {
-            document.getElementById("textCopied2").innerHTML = '';
-        }, 1000);
-  });
+    });
+}
+
+createClickToCopy("folderPath", "textCopied", folderPath);
+createClickToCopy("folderPath2", "textCopied2", folderPath2);
+createClickToCopy("folderPath3", "textCopied3", folderPath3);
+
+/////////////////////////

3251ca13654be9450d3a44208b96e868345a6130
Alistair Bahr <alistairbahr@outlook.com>
2022-09-23 20:07:09 +0200

    update old-reddit-centered-layout.user.js

--- old-reddit-centered-layout.user.js
+++ old-reddit-centered-layout.user.js
@@ -1,24 +1,32 @@
 // ==UserScript==
 // @name         old reddit centered layout
 // @namespace    https://greasyfork.org/en/users/12725-alistair1231
-// @version      0.2
+// @version      0.3
 // @description  makes old reddit centered instead of left-aligned
 // @author       Alistair1231
 // @match        https://old.reddit.com/r/*
+// @match        https://old.reddit.com/
 // @icon         https://icons.duckduckgo.com/ip2/reddit.com.ico
 // @downloadURL  https://github.com/Auncaughbove17/my-userscripts/raw/main/old-reddit-centered-layout.user.js
 // @grant        none
 // @license GPL-3.0
 // ==/UserScript==
 
-(function() {
+function fixMainPage(){
+    document.querySelector(".sitetable").style = "margin:auto;width:60%;";
+}
+function fixCommentPage(){
+    document.querySelector(".res .commentarea > .usertext").style = "margin-top:0px;";
+    document.querySelector("div.content").style = "margin:auto;width:60%;";
+}
+
+(function () {
     'use strict';
-
-    // fix comment area not being completely visible
-    document.querySelector(".res .commentarea > .usertext").style="margin-top:0px;";
-    // make the page centered
-    document.querySelector("div.content").style="margin:auto;width:50%;";
-
-
-
+    
+    if(document.URL.startsWith("https://old.reddit.com/r/"))
+        fixCommentPage();
+    else
+       fixMainPage();
 })();
+
+

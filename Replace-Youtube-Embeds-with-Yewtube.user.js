d328b138c9bb33158d964000f710c861976cc2f4
Alistair1231 <alistairbahr@outlook.com>
2023-04-24 19:19:39 +0200

    Update Replace-Youtube-Embeds-with-Yewtube.user.js

--- Replace-Youtube-Embeds-with-Yewtube.user.js
+++ Replace-Youtube-Embeds-with-Yewtube.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Replace Youtube Embeds with Yewtube (invidious) Embeds
 // @namespace    https://github.com/Auncaughbove17/my-userscripts/
-// @version      0.1.2
+// @version      0.1.3
 // @description  Replaces youtube embeds with yewtube (invidious) embeds
 // @author       Alistair1231
 // @match        http*://*/*
@@ -12,7 +12,7 @@
 // https://greasyfork.org/en/scripts/464638-replace-youtube-embeds-with-yewtube-invidious-embeds
 const qSA = x => document.querySelectorAll(x);
 const replaceIframe = (iframe) => {
-    iframe.src = iframe.src.replace("https://www.youtube.com/embed", "https://yewtu.be/embed");
+    iframe.src = iframe.src.replace("youtube.com/embed", "yewtu.be/embed");
     iframe.setAttribute("allowfullscreen", "allowfullscreen");
     iframe.setAttribute("mozallowfullscreen", "mozallowfullscreen");
     iframe.setAttribute("msallowfullscreen", "msallowfullscreen");
@@ -27,14 +27,14 @@ const replaceIframe = (iframe) => {
         mutations.forEach(function (mutation) {
             if (mutation.type == "childList") {
                 // check for new youtube embeds and replace them with invidious embeds
-                qSA("iframe[src*='https://www.youtube.com/embed']").forEach(replaceIframe);
+                qSA("iframe[src*='youtube.com/embed']").forEach(replaceIframe);
             }
 
         });
     });
 
     // run this once for each iframe that is already on the page
-    qSA("iframe[src*='https://www.youtube.com/embed']").forEach(replaceIframe);
+    qSA("iframe[src*='youtube.com/embed']").forEach(replaceIframe);
 
     // keep observing for new iframes and run code from above
     observer.observe(document, { childList: true, subtree: true });

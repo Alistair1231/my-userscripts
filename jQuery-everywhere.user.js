287630e7d6a8ea8593cfe83b3126cb6f3cdf9ff9
Alistair1231 <alistairbahr@outlook.com>
2023-04-25 21:54:35 +0200

    Update jQuery-everywhere.user.js

--- jQuery-everywhere.user.js
+++ jQuery-everywhere.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         jQuery and common function shortcuts everywhere
 // @namespace    https://github.com/Alistair1231/my-userscripts/
-// @version      0.4.1
+// @version      0.4.2
 // @description  injects jquery if not exists
 // @author       Alistair1231
 // @match      *://*/*
@@ -12,11 +12,34 @@
 // https://greasyfork.org/en/scripts/439017-jquery-and-common-function-shortcuts-everywhere
 (function () {
   'use strict';
-  let e = document.createElement('script');
-  e.id = 'injected-script';
-  e.innerText = "";
-  document.head.appendChild(e);
 
+  const helpString = `
+al: jQuery and Method shortcuts everywhere
+------------------------------------------
+al.js(obj) - JSON.stringify(obj)
+al.jsp(obj) - JSON.stringify(obj, null, 2)
+al.jp(str) - JSON.parse(str)
+al.qs(selector) - document.querySelector(selector)
+al.qsa(selector) - document.querySelectorAll(selector)
+al.gid(id) - document.getElementById(id)
+------------------------------------------`;
+
+  const shortcuts = `
+const al = {
+  help: () => console.log('${helpString}'),
+  js: (obj) => JSON.stringify(obj),
+  jsp: (obj) => JSON.stringify(obj, null, 2),
+  jp: (str) => JSON.parse(str),
+  qs: (selector) => document.querySelector(selector),
+  qsa: (selector) => document.querySelectorAll(selector),
+  gid: (id) => document.getElementById(id)
+};
+`;
+
+  let e = document.createElement('script');
+  e.id = 'injectedScript';
+  e.innerText = shortcuts;
+  document.head.appendChild(e);
 
   if (typeof jQuery == 'undefined') {
     // https://stackoverflow.com/questions/54499985/how-can-i-load-an-external-script-on-a-webpage-using-tampermonkey
@@ -31,41 +54,5 @@
     });
   }
 
-  function removeIndent(str) {
-    const lines = str.split('\n');
-    if (lines[0] !== '' || lines[lines.length - 1] !== '') {
-      return str;
-    }
-    lines.shift();
-    lines.pop();
 
-    const lens = lines.map(l => l.match(/ */)[0].length)
-    const minLen = Math.min(...lens);
-    return '\n' + lines.map(l => l.substr(minLen)).join('\n') + '\n';
-  }
-
-  const helpString = removeIndent`
-    al: jQuery and Method shortcuts everywhere
-    ------------------------------------------
-    al.js(obj) - JSON.stringify(obj)
-    al.jsp(obj) - JSON.stringify(obj, null, 2)
-    al.jp(str) - JSON.parse(str)
-    al.qs(selector) - document.querySelector(selector)
-    al.qsa(selector) - document.querySelectorAll(selector)
-    al.gid(id) - document.getElementById(id)
-    ------------------------------------------`;
-
-  const shortcuts = removeIndent`
-    const al = {
-      help: () => console.log(${helpString}),
-      js: (obj) => JSON.stringify(obj),
-      jsp: (obj) => JSON.stringify(obj, null, 2),
-      jp: (str) => JSON.parse(str),
-      qs: (selector) => document.querySelector(selector),
-      qsa: (selector) => document.querySelectorAll(selector),
-      gid: (id) => document.getElementById(id),
-    };
-`;
-
-  e.innerText += shortcuts;
 })();

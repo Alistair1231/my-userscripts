ac985122992c0b54d0913dc504c1316a476989d6
Alistair1231 <alistairbahr@outlook.com>
2023-04-28 20:38:11 +0200

    Update jQuery-and-common-function-shortcuts-everywhere.user.js

--- jQuery-and-common-function-shortcuts-everywhere.user.js
+++ jQuery-and-common-function-shortcuts-everywhere.user.js
@@ -4,30 +4,18 @@
 // @version      0.4.5
 // @description  injects jquery if not exists and adds some common function shortcuts to the window object. See al.help() for details.
 // @author       Alistair1231
-// @match      *://*/*
+// @match        *://*/*
 // @downloadURL  https://github.com/Alistair1231/my-userscripts/raw/main/jQuery-and-common-function-shortcuts-everywhere.user.js
 // @grant        GM_xmlhttpRequest
-// @license GPL-3.0
+// @license      GPL-3.0
 // ==/UserScript==
 // https://greasyfork.org/en/scripts/439017-jquery-and-common-function-shortcuts-everywhere
 (function () {
   'use strict';
 
-  const helpString = `
-al: jQuery and Method shortcuts everywhere
-------------------------------------------
-al.cl(str) - console.log(str)
-al.js(obj) - JSON.stringify(obj)
-al.jsp(obj) - JSON.stringify(obj, null, 2)
-al.jp(str) - JSON.parse(str)
-al.qs(selector) - document.querySelector(selector)
-al.qsa(selector) - document.querySelectorAll(selector)
-al.gid(id) - document.getElementById(id)
-------------------------------------------`;
-
   const shortcuts = `
-const al = {
-  help: () => console.log(\`${helpString}\`),
+const bl = {
+  help: () => console.log('al: jQuery and Method shortcuts everywhere\n------------------------------------------\nal.cl(str) - console.log(str)\nal.js(obj) - JSON.stringify(obj)\nal.jsp(obj) - JSON.stringify(obj, null, 2)\nal.jp(str) - JSON.parse(str)\nal.qs(selector) - document.querySelector(selector)\nal.qsa(selector) - document.querySelectorAll(selector)\nal.gid(id) - document.getElementById(id)\n------------------------------------------'),
   cl: (str) => console.log(str),
   js: (obj) => JSON.stringify(obj),
   jsp: (obj) => JSON.stringify(obj, null, 2),
@@ -38,22 +26,9 @@ const al = {
 };
 `;
 
-  let e = document.createElement('script');
+  const e = document.createElement('script');
   e.id = 'injectedScript';
   e.innerText = shortcuts;
   document.head.appendChild(e);
-
-  if (typeof jQuery == 'undefined') {
-    // https://stackoverflow.com/questions/54499985/how-can-i-load-an-external-script-on-a-webpage-using-tampermonkey
-    GM_xmlhttpRequest({
-      method: "GET",
-      // from other domain than the @match one (.org / .com):
-      url: "https://code.jquery.com/jquery-3.6.0.min.js",
-      onload: (ev) => {
-        e.innerText += ev.responseText;
-      }
-    });
-  }
-
 
 })();

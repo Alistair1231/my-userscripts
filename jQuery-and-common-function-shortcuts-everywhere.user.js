218c51c4c24645dad6945e507a1514aeff463417
Alistair1231 <alistairbahr@outlook.com>
2023-04-28 23:25:29 +0200

    Update jQuery-and-common-function-shortcuts-everywhere.user.js

--- jQuery-and-common-function-shortcuts-everywhere.user.js
+++ jQuery-and-common-function-shortcuts-everywhere.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         jQuery and common function shortcuts everywhere
 // @namespace    https://github.com/Alistair1231/my-userscripts/
-// @version      0.4.6
+// @version      0.4.7
 // @description  injects jquery if not exists and adds some common function shortcuts to the window object. See al.help() for details.
 // @author       Alistair1231
 // @match        *://*/*
@@ -12,7 +12,9 @@
 // https://greasyfork.org/en/scripts/439017-jquery-and-common-function-shortcuts-everywhere
 (function () {
   'use strict';
-const helpString = 'al: jQuery and Method shortcuts everywhere\\n\
+const helpString = `
+'\
+al: jQuery and Method shortcuts everywhere\\n\
 ------------------------------------------\\n\
 al.cl(str) - console.log(str)\\n\
 al.js(obj) - JSON.stringify(obj)\\n\
@@ -22,9 +24,11 @@ al.qs(selector) - document.querySelector(selector)\\n\
 al.qsa(selector) - document.querySelectorAll(selector)\\n\
 al.gid(id) - document.getElementById(id)\\n\
 al.print() - Prints the object definition\\n\
-------------------------------------------\\n';
+------------------------------------------\\n\
+'
+`;
 
-  const shortcuts = `
+const shortcuts = `
 const al = {
   help: () => console.log(${helpString}),
   cl: (str) => console.log(str),
@@ -33,15 +37,21 @@ const al = {
   jp: (str) => JSON.parse(str),
   qs: (selector) => document.querySelector(selector),
   qsa: (selector) => document.querySelectorAll(selector),
-  gid: (id) => document.getElementById(id)
+  gid: (id) => document.getElementById(id),
+  print: () => {
+    const { help, ...rest } = al;
+    console.log(rest);
+  }
 };
 `;
 
   const e = document.createElement('script');
   e.id = 'injectedScript';
   e.innerText = shortcuts;
-  // append console.log(shortcuts) as al.print() to the script
-  e.innerText += `al.print = () => console.log(${shortcuts})`;
+  
+
+
+  
   document.head.appendChild(e);
 
 })();

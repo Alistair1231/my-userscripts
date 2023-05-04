dae976f5d03601e082fc9dc353ebff11293e81e7
Alistair1231 <alistairbahr@outlook.com>
2023-05-04 19:15:57 +0200

    Update iggAutoContinue.user.js

--- iggAutoContinue.user.js
+++ iggAutoContinue.user.js
@@ -1,37 +1,33 @@
 // ==UserScript==
 // @name         igg-games/bluemediafiles auto continue after wait
 // @namespace    https://github.com/Alistair1231/my-userscripts/
-// @version      0.4.5
+// @version      0.5
 // @description  waits for cooldown and clicks continue
 // @author       Alistair1231
-// @match        http*://bluemediafiles.com/url-generator.php?url=*
-// @match        http*://bluemediafiles.net/url-generator.php?url=*
-// @match        http*://bluemediafiles.eu/url-generator.php?url=*
-// @match        http*://bluemediafiles.homes/url-generator.php?url=*
-// @match        http*://bluemediafile.sbs/url-generator.php?url=*
-// @match        http*://bluemediafile.site/url-generator.php?url=*
+// @match        http*://*/url-generator.php?url=*
 // @downloadURL  https://github.com/Alistair1231/my-userscripts/raw/main/iggAutoContinue.user.js
 // @grant        none
 // @license GPL-3.0
 // ==/UserScript==
 
 
-(function() {
+(function () {
     'use strict';
-    // waiting for countown
-    var clickButton;
-    var count=0;
-    var interval = setInterval(() =>
-    {
-        // countdown = document.getElementsByClassName("item")[0].children[0].innerHTML;
-        clickButton = document.getElementById("nut");
-        count++;
-        console.log("Waited "+count+" seconds");
-        console.log("image is: "+clickButton.src);
-        if(clickButton.src!==""){
-            //clicking
-            clickButton.parentElement.submit();
-            clearInterval(interval);
-        }
-    }, 1000);
+    if (window.location.href.includes("bluemediafile")) {
+        // waiting for countown
+        var clickButton;
+        var count = 0;
+        var interval = setInterval(() => {
+            // countdown = document.getElementsByClassName("item")[0].children[0].innerHTML;
+            clickButton = document.getElementById("nut");
+            count++;
+            console.log("Waited " + count + " seconds");
+            console.log("image is: " + clickButton.src);
+            if (clickButton.src !== "") {
+                //clicking
+                clickButton.parentElement.submit();
+                clearInterval(interval);
+            }
+        }, 1000);
+    }
 })();

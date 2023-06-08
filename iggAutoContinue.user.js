2dbd927a2bca65dcdcf711f6bdfda52b041a31a3
Alistair Bahr <alistair.bahr@outlook.com>
2023-06-08 22:02:37 +0200

    update iggAutoContinue.user.js

--- iggAutoContinue.user.js
+++ iggAutoContinue.user.js
@@ -1,26 +1,24 @@
 // ==UserScript==
 // @name         igg-games/bluemediafiles auto continue after wait
 // @namespace    https://github.com/Alistair1231/my-userscripts/
-// @version      0.5
+// @version      0.6
 // @description  waits for cooldown and clicks continue
 // @author       Alistair1231
-// @match        http*://*/url-generator.php?url=*
+// @match        http*://*/*
 // @license      MIT
 // ==/UserScript==
 // https://greasyfork.org/en/scripts/425739-igg-games-bluemediafiles-auto-continue-after-wait
 // https://openuserjs.org/scripts/Alistair1231/igg-gamesbluemediafiles_auto_continue_after_wait/
 (function () {
     'use strict';
-    if (window.location.href.includes("bluemediafile")) {
+    if (window.location.href.match((/.*bluemedia.*url-generator-?\d+?.php/)).length>0) {
         // waiting for countown
         var clickButton;
         var count = 0;
         var interval = setInterval(() => {
-            // countdown = document.getElementsByClassName("item")[0].children[0].innerHTML;
             clickButton = document.getElementById("nut");
             count++;
             console.log("Waited " + count + " seconds");
-            console.log("image is: " + clickButton.src);
             if (clickButton.src !== "") {
                 //clicking
                 clickButton.parentElement.submit();

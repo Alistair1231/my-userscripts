3a8b00211d1e41be39892073b1d84b77d6e34d2f
Alistair1231 <alistair.bahr@outlook.com>
2023-07-07 15:45:08 +0200

    Update MAM-folder-path.user.js

--- MAM-folder-path.user.js
+++ MAM-folder-path.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MAM folder path
 // @namespace    https://github.com/Alistair1231/my-userscripts/
-// @version      0.5.4
+// @version      0.5.5
 // @description  Add Audiobook folder path to torrent info
 // @author       Alistair1231
 // @include      https://www.myanonamouse.net/t/*

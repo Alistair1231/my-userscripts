b91c96721568d1f2e21c1e400c38e10ecfb0e2b3
Alistair Bahr <alistair.bahr@outlook.com>
2023-12-15 21:43:41 +0100

    add center content for cheatsheet monster

---
+++ cheatsheet-monster-centered.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Center content on cheatsheet monster page
+// @namespace    https://github.com/Alistair1231/my-userscripts/
+// @version      0.0.5
+// @description  Center content on cheatsheet monster page
+// @author       Alistair1231
+// @icon         https://icons.duckduckgo.com/ip2/cheatsheet.monster.ico
+// @grant        none
+// @match        https://cheatsheet.monster/*
+// @license MIT
+// ==/UserScript==
+
+// body { margin: auto; }
+
+(function () {
+document.body.style.margin = "auto";
+})();

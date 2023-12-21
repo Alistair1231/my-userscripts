05fbe17d8c4ec4cdba08ac829ecc1298af51e4d8
Alistair Bahr <alistair.bahr@outlook.com>
2023-12-21 15:09:53 +0100

    hideoutshowcase

---
+++ hideoutshowcase-highResImages.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         High res images on hideoutshowcase
+// @namespace    https://github.com/Alistair1231/my-userscripts/
+// @version      0.1
+// @description  replaces low res images on hideoutshowcase with high res ones
+// @author       Alistair1231
+// @match        https://hideoutshowcase.com/*
+// @icon         https://icons.duckduckgo.com/ip2/hideoutshowcase.com.ico
+// @license      MIT
+// ==/UserScript==
+
+const replaceAllImages = () => {
+  [...document.querySelectorAll("div a.nk-post-image img")].map(
+    (x) => (x.src = x.src.replace("/other", "/gallery"))
+  );
+};
+
+const observer = new MutationObserver(replaceAllImages);
+observer.observe(document.body, { childList: true, subtree: true });

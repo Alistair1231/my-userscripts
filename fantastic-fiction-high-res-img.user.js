d29f78fcb526ef896004704b1fa4170f9c0fe715
Alistair Bahr <alistair.bahr@outlook.com>
2023-12-28 23:37:50 +0100

    High res images on fantasticfiction.com

---
+++ fantastic-fiction-high-res-img.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         High res images on fantasticfiction.com
+// @namespace    https://github.com/Alistair1231/my-userscripts/
+// @version      0.1
+// @description  replaces low res images on hideoutshowcase with high res ones
+// @author       Alistair1231
+// @match        https://www.fantasticfiction.com/*
+// @icon         https://icons.duckduckgo.com/ip2/fantasticfiction.com.ico
+// @license      MIT
+// ==/UserScript==
+
+// From:    https://m.media-amazon.com/images/I/516s5YwK2HL.SX316.SY480._SL500_.jpg
+// To:      https://m.media-amazon.com/images/I/516s5YwK2HL.jpg
+const replaceAllImages = () => {
+  [...document.querySelectorAll("a img[alt='thumb']")].map(
+    (x) => (x.src = x.src.replace(/\.SX\d+\.SY\d+.*?(?=\.jpg)/gm, ""))
+  );
+};
+
+const observer = new MutationObserver(replaceAllImages);
+observer.observe(document.body, { childList: true, subtree: true });

4c0925978906d6f6412bf4ec049c8252c3012220
Alistair Bahr <alistair.bahr@outlook.com>
2023-12-29 00:25:29 +0100

    modified:   fantastic-fiction-high-res-images.user.js

--- fantastic-fiction-high-res-images.user.js
+++ fantastic-fiction-high-res-images.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         High res images on fantasticfiction.com
 // @namespace    https://github.com/Alistair1231/my-userscripts/
-// @version      0.2.1
+// @version      0.3.0
 // @description  replaces low res images on hideoutshowcase with high res ones
 // @author       Alistair1231
 // @match        https://www.fantasticfiction.com/*
@@ -14,14 +14,41 @@
   // From:    https://m.media-amazon.com/images/I/516s5YwK2HL.SX316.SY480._SL500_.jpg
   // To:      https://m.media-amazon.com/images/I/516s5YwK2HL.jpg
   // https://regex101.com/r/yoj8UD/1
-  const replaceAllImages = () => {
-    [...document.querySelectorAll("img")].map(
-      (x) => (x.src = x.src.replace(/(?:[\._]+?S[XLY]\d+_?){3}(?=\.jpg)/gm, ""))
-    );
+  const regex = /(?:[\._]+?S[XLY]\d+_?){3}(?=\.jpg)/gm;
+
+  const isAlreadyProcessed = (src) => !regex.test(src);
+
+  const replaceSrc = (img) => {
+    const newSrc = img.src.replace(regex, "");
+    if (img.src !== newSrc) {
+      img.src = newSrc;
+    }
   };
 
-  const observer = new MutationObserver(replaceAllImages);
-  observer.observe(document.body, { childList: true, subtree: true });
+  const replaceAllImages = () => {
+    document.querySelectorAll("img").forEach(replaceSrc);
+  };
 
+  const replaceMutation = (mutationList) => {
+    mutationList.forEach((mutation) => {
+      if (mutation.type === "attributes" && mutation.attributeName === "src") {
+        const img = mutation.target;
+        if (!isAlreadyProcessed(img.src)) {
+          replaceSrc(img);
+        }
+      }
+    });
+  };
+
+  const observer = new MutationObserver(replaceMutation);
+
+  // Observe all src attribute changes
+  observer.observe(document.body, {
+    attributes: true,
+    subtree: true,
+    attributeFilter: ["src"],
+  });
+
+  // Run the function on initial load
   replaceAllImages();
 })();

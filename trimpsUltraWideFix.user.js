fc0abcbea1553edf85ce98799a959cd67eb92cac
Alistair Bahr <alistair.bahr@outlook.com>
2024-03-27 21:37:30 +0100

    trimpsUltraWideFix.user.js

--- trimpsUltraWideFix.user.js
+++ trimpsUltraWideFix.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Trimps Ultra Wide Fix
 // @namespace    https://github.com/Alistair1231/my-userscripts/
-// @version      0.1.1
+// @version      0.1.2
 // @description  loads Trimps in an iframe with 16:10 aspect ratio
 // @downloadURL  https://github.com/Alistair1231/my-userscripts/raw/master/trimpsUltraWideFix.user.js
 // @author       Alistair1231
 // @match        https://trimps.github.io/
 // @icon         https://icons.duckduckgo.com/ip2/github.io.ico
 // @grant        GM.addStyle
+// @grant        unsafeWindow
 // @license MIT
 // ==/UserScript==
 // https://github.com/Alistair1231/my-userscripts/raw/master/trimpsUltraWideFix.user.js
@@ -15,6 +16,8 @@
 
 (() => {
   'use strict';
+
+
   //only run if aspect ratio is wider than 16:9
   if ((window.innerWidth / window.innerHeight) > (16 / 9)) {
     return;
@@ -29,9 +32,12 @@
   // load current page in iframe with dimensions 16:10
   const iframe = document.createElement('iframe');
   iframe.src = location.href;
-  // calculate 16:10 dimensions that fill the viewport vertically
-  iframe.width = Math.ceil(window.innerHeight * 16 / 10);
-  iframe.height = window.innerHeight;
+
+  // set iframe size, hieght = window.innerHeight, width 100% unless it is wider than 16:10
+  iframe.style.height = '100vh';
+  iframe.style.width = '100%';
+  iframe.style.maxWidth = '160vh'; // 16:10 aspect ratio (160vh = 16/10 * 100vh)
+
   // center iframe horizontally
   iframe.style.position = 'absolute';
   iframe.style.left = '50%';

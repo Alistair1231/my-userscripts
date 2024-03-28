87cdc48188614d631ec9573ad107bf56676f57b1
Alistair Bahr <alistair.bahr@outlook.com>
2024-03-28 20:00:19 +0100

    trimps-tweaks.user.js

--- trimps-tweaks.user.js
+++ trimps-tweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Trimps Tweaks
 // @namespace    https://github.com/Alistair1231/my-userscripts/
-// @version      0.1.3
+// @version      0.2.0
 // @description  various tweaks for Trimps
 // @downloadURL  https://github.com/Alistair1231/my-userscripts/raw/master/trimps-tweaks.user.js
 // @author       Alistair1231
@@ -11,21 +11,33 @@
 // ==/UserScript==
 // https://github.com/Alistair1231/my-userscripts/raw/master/trimps-tweaks.user.js
 
-const addSaveLabel = () => {
+const getSave = () => window.save(true);
+const setSave = (save) => window.load(save);
+
+const copy = async (text) => await window.navigator.clipboard.writeText(text);
+const paste = () => prompt('Paste save here');
+
+const addHotkey = (key, action) => {
+  document.addEventListener('keydown', (e) => {
+    if (e.key === key) action(e);
+  });
+}
+
+const addLabel = (selector,filter,value) => {
   const run = () => {
-    const exportDiv = [...document.querySelectorAll("#settingsTable div")].find(x => x.innerHTML === "Export");
+    const exportDiv = [...document.querySelectorAll(selector)].find(x => x.innerHTML === filter);
     if (exportDiv) {
-      exportDiv.innerHTML += " (F8)";
+      exportDiv.innerHTML += value;
       return true;
     }
     return false;
   }
   // try to run immediately, if it fails, wait for the settings to load
-  if(run()) return;
+  if (run()) return;
 
   new Promise(resolve => {
     const observer = new MutationObserver(() => {
-      const target = document.querySelector("#settingsTable div");
+      const target = document.querySelector(selector);
       if (target) {
         observer.disconnect();
         resolve();
@@ -34,26 +46,24 @@ const addSaveLabel = () => {
     observer.observe(document.body, {childList: true, subtree: true});
   }).then(run);
 }
+
+
 (async () => {
   'use strict';
   // copy save to clipboard when pressing F8
-  document.addEventListener('keydown', (e) => {
-    if (e.key === 'F8') {
-      e.preventDefault();
-      navigator.clipboard.writeText(window.save(true));
-    }
+  addHotkey('F8', (e) => {
+    e.preventDefault();
+    copy(getSave());
+  });
+
+  addHotkey('F9', async (e) => {
+    e.preventDefault();
+    console.log('Backup save:', getSave());
+    setSave(paste());
   });
 
   // add (F8) to the export button
-  addSaveLabel();
-
-  // when pressing F5, first autoSave() then reload the page
-  document.addEventListener('keydown', (e) => {
-    if (e.key === 'F5') {
-      e.preventDefault();
-      window.autoSave();
-      window.location.reload();
-    }
-  });
+  addLabel("#settingsTable div","Export"," (F8)");
+  addLabel("#settingsTable div","Import"," (F9)");
 
 })();

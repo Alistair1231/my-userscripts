d0d62728d9d607a7ccb68c0a8a1bbcd545c39851
Alistair <alistair.bahr@outlook.com>
2024-12-21 13:23:45 +0100

    add proper overlay to setup in evolve idle script

--- EvolveIdleSavegameBackup.user.js
+++ EvolveIdleSavegameBackup.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Evolve Idle Cloud Save
 // @namespace    https://github.com/Alistair1231/my-userscripts/
-// @version      0.2.0
+// @version      0.3.0
 // @description  Automatically upload your evolve save to a gist
 // @downloadURL  https://github.com/Alistair1231/my-userscripts/raw/master/EvolveIdleSavegameBackup.user.js
 // @author       Alistair1231
@@ -17,33 +17,51 @@
 
 /*
 ## Why This?
-I lost my save game 😞. To prevent this from happening again, I implemented a quick and dirty cloud backup using a GitHub Gist. 
-
-Only export is implemented currently, but I might add an import feature later. Don't hold your breath though. 🤷
+I lost my save game 😞. To prevent this from happening again, I implemented a quick and dirty cloud 
+backup using a GitHub Gist. 
 
 ## What does it do?
-It makes a backup every 30 minutes (adjustable at the bottom of the script). The save game will be written to a gist, that is defined by the user. By using a gist, you also get the benefit of versioning. So you can roll back to earlier saves. 😉 
-Currently, it dumbly overwrites the file with the same name, so be careful with that. I recommend using a separate file for each PC, otherwise your savegame might get overwritten by another PC, which has the tab open in the background. Also, only have one tab open at a time, otherwise the save might get overwritten by the other tab. This is easily recoverable, using the revision history of the gist, but it is worth mentioning. 😅
+It makes a backup every 30 minutes (adjustable at the bottom of the script). The save game will 
+be written to a gist, that is defined by the user. By using a gist, you also get the benefit of 
+versioning. So you can roll back to earlier saves. 😉 
 
-## Security Concerns
-**The API token is saved as plaintext**, so be sure to only give it the gist scope and use a token for this purpose alone so you can revoke it if necessary. Better safe than sorry. If you are really paranoid, make a GitHub account only for this purpose. I don't want to implement encryption, that sounds like a hassle, and then you have the new issue of loosing that password as well as having to always enter it as saving the password next to the encrypted content pretty much defeats the purpose. 😁
+I recommend using a separate file for each PC, otherwise your savegame might get overwritten by 
+another PC, which has the tab open in the background. Also, only have one tab open at a time, 
+otherwise the save might get overwritten by the other tab. This is easily recoverable, using the 
+revision history of the gist, but it is worth mentioning. 😅
 
 ## How to use?
-On first setup you will need to manually create a GitHub API key and a Gist. That is described in an alert box when you first use the script, but since I think I remember Google having plans to remove those, I will put the description here as well.  
-Be aware, I usually forget to update these descriptions, so if in doubt look at the code.  
-Also, this is only tested on [Violentmonkey](https://violentmonkey.github.io/get-it/) and Firefox. The Browser should not matter, but some other Userscript managers might handle the GM functions differently. If something does not work, try to use Violentmonkey, or message me, if I have time I might try to help.  
-Also, I export the `makeBackup` function to the global scope, so you can manually trigger a backup by typing `makeBackup()` in the console. This is useful if you want to make a backup before you close the tab, but don't want to wait for the next interval. In the future I might add a button to the UI. Maybe together with a button to import a save game. 🤔
+On first setup you will need to manually create a GitHub API key and a Gist and then input them 
+for the script to use.
+This is only tested on [Violentmonkey](https://violentmonkey.github.io/get-it/). Some other 
+Userscript managers might handle the GM functions differently. If something does not work, 
+try to use Violentmonkey. 😊
+Also, I export the `makeBackup` function to the global scope, so you can manually trigger a 
+backup by typing `makeBackup()` in the dev console of your browser (`Ctrl/CMD + Shift + J` to open).
 
 ## Setup instructions 
 
-You will need a GistID and a Personal Access Token to use this.
+You will need a GistID and a Personal Access Token with `Gist` scope to use this.
 
-Create a gist, the description does not matter, in it make a file e.g. called "save.txt" and add some random content so GitHub doesn't complain, you can do that here: https://gist.github.com
-Afterwards, you can find the GistID in the URL: https://gist.github.com/{{Username}}/{{GistID}}
+Create a gist, the description does not matter, in this Gist, create a file e.g. called "save.txt", 
+add some random content and save. You can do that here (You will need a GitHub account): 
+https://gist.github.com
+Afterwards, you can find the GistID in the URL: https://gist.github.com/{Username}/{GistID}
 
-The Personal Access Token you have to create here: https://github.com/settings/tokens you only need the gist scope.
+The Personal Access Token you have to create here: https://github.com/settings/tokens you only 
+need the gist scope.
 
-If you make a mistake you should be asked again, alternatively you can manually set these values in the Userscript storage.
+If you make a mistake you should be asked again, alternatively you can manually set these values 
+in the Userscript storage. In Violentmonkey you can access this by clicking on the extension icon,
+then right-clicking on the script and selecting `Values`. There we want values like this (these are 
+random examples I didn't leak my credentials 😉):
+```
+{
+filename: "save.txt",
+gistId: "856ce06ecda1234e095c156da8fd44d7",
+token: "ghp_k928znRUu7ZI0tySv9gP2A2x9VdvVLrmrXCD"
+}
+```
 
 ## How does it work? (technical)
 The script makes use of `GM.xmlhttpRequest` for the request and `GM.setValue`/`GM.getValue` for storing/retrieving the secrets. The timing is done with `setInterval`. For saving the Data the GitHub API is used. The save game is exported using the `exportGame` function, which is exposed by the game. The save game is then sent to the GitHub API using a PATCH request. 
@@ -104,30 +122,100 @@ const getSecrets = async () => {
   };
 };
 
+// Create an overlay to collect secrets from the user
+const createSecretsOverlay = () => {
+  const overlay = document.createElement('div');
+  overlay.style.position = 'fixed';
+  overlay.style.top = '0';
+  overlay.style.left = '0';
+  overlay.style.width = '100%';
+  overlay.style.height = '100%';
+  overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+  overlay.style.display = 'flex';
+  overlay.style.alignItems = 'center';
+  overlay.style.justifyContent = 'center';
+  overlay.style.zIndex = '1000';
+
+  const modal = document.createElement('div');
+  modal.style.backgroundColor = 'white';
+  modal.style.padding = '20px';
+  modal.style.borderRadius = '8px';
+  modal.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.2)';
+  modal.style.width = '400px';
+
+  const guideText = document.createElement('p');
+  guideText.textContent = `You will need a GistID and a Personal Access Token to use 
+  this cloud-save script. They will be saved as cleartext in the Userscript storage!
+  \n\n
+  Create a gist at https://gist.github.com and make note of the GistID in the URL (e.g., 
+  https://gist.github.com/{{Username}}/{{GistID}}). The script doesn't handle creating files,
+  so create a file in the gist with the filename you wish to use and add some random content.
+  \n\n
+  Create a Personal Access Token at 
+  https://github.com/settings/tokens with the "gist" scope.`;
+  guideText.style.fontSize = '14px';
+  guideText.style.marginBottom = '10px';
+
+  const form = document.createElement('form');
+  form.style.display = 'flex';
+  form.style.flexDirection = 'column';
+
+  const createInput = (labelText, placeholderText) => {
+    const label = document.createElement('label');
+    label.textContent = labelText;
+    label.style.marginTop = '10px';
+    label.style.fontSize = '14px';
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = placeholderText;
+    input.style.marginTop = '5px';
+    input.style.padding = '8px';
+    input.style.fontSize = '14px';
+
+    form.appendChild(label);
+    form.appendChild(input);
+    return input;
+  };
+
+  const gistIdInput = createInput('Gist ID', 'Enter your Gist ID');
+  const tokenInput = createInput('GitHub Personal Access Token', 'Enter your Token');
+  const fileNameInput = createInput('Filename', 'Enter filename (default: save.txt)');
+
+  const button = document.createElement('button');
+  button.textContent = 'Save';
+  button.style.marginTop = '15px';
+  button.style.padding = '10px';
+  button.style.fontSize = '14px';
+  button.style.cursor = 'pointer';
+
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    const gistId = gistIdInput.value.trim();
+    const token = tokenInput.value.trim();
+    const fileName = fileNameInput.value.trim() || 'save.txt';
+
+    if (!gistId || !token) {
+      alert('Gist ID and Token are required!');
+      return;
+    }
+
+    GM.setValue('gistId', gistId);
+    GM.setValue('token', token);
+    GM.setValue('filename', fileName);
+
+    document.body.removeChild(overlay);
+  });
+
+  form.appendChild(button);
+  modal.appendChild(guideText);
+  modal.appendChild(form);
+  overlay.appendChild(modal);
+  document.body.appendChild(overlay);
+};
+
 const askForSecrets = () => {
-  // Explain to the user what's happening
-  alert(`You will need a GistID and a Personal Access Token to use this. They will be saved as cleartext in the Userscipt storage!
-
-Create a gist, the description does not matter, in it make a file and make note of the filename e.g. "save.txt" and add some random content so github doesn't complain, you can do that here:
-https://gist.github.com
-then you can find the GistID in the URL: 
-https://gist.github.com/{{Username}}/{{GistID}}
-
-The Personal Access Token you have to create here: 
-https://github.com/settings/tokens
-you only need the gist scope.
-
-If you make a mistake you should be asked again, alternatively you can manually set these values in the Userscript storage.`);
-
-  const gistId = prompt("Enter your Gist ID");
-  const token = prompt("Enter your GitHub Personal Access Token");
-  const fileName = prompt(`Enter the filename that should be used for the save game, or leave empty for 'save.txt'
-I recommend using a separate file for each PC, otherwise your savegame might get overwritten by another PC, which has the tab open in the background.`
-  );
-  GM.setValue("gistId", gistId);
-  GM.setValue("token", token);
-  // if the user left the filename empty, use the default
-  GM.setValue("filename", fileName || "save.txt");
+  createSecretsOverlay();
 };
 
 const tryGetSecrets = async () => {

3b7eb85f99e4efd483d73363c607bbde99201c08
Alistair1231 <alistair.bahr@outlook.com>
2025-02-21 21:03:34 +0100

    Update dark-mode-toggle.user.js

--- dark-mode-toggle.user.js
+++ dark-mode-toggle.user.js
@@ -85,7 +85,7 @@
               -webkit-filter: invert(${CONFIG.inversionPercent}%);
               filter: invert(${CONFIG.inversionPercent}%);
           }
-          img, video, picture, iframe, object, embed, canvas {
+          img, video, picture, iframe, object, embed, canvas, svg {
               -webkit-filter: invert(${CONFIG.mediaInversionPercent}%);
               filter: invert(${CONFIG.mediaInversionPercent}%);
           }

a1bd14a80ef2fc6c49d4a82491fff174868d0f94
Alistair1231 <alistair.bahr@outlook.com>
2025-05-08 14:45:19 +0200

    Update youtube-music-high-quality-video.user.js

--- youtube-music-high-quality-video.user.js
+++ youtube-music-high-quality-video.user.js
@@ -11,13 +11,22 @@
 // https://greasyfork.org/en/scripts/535336-youtube-music-high-quality-video
 // https://github.com/Alistair1231/my-userscripts/blob/master/youtube-music-high-quality-video.user.js
 
-// auto / highres (8k or 'original') / hd2880 / hd2160 / hd1440 / hd1080 / hd720 / large / medium / small / tiny
-var setQuality = (quality) => document.getElementById("movie_player").setPlaybackQualityRange(quality)
-var checkElement = () => {
-	element = document.getElementById("movie_player")
-  if(!element)
-		setTimeout(checkElement,500);
-	else{
-		setQuality("hd1440")
-  }
-}
+(function () {
+  "use strict";
+  // auto / highres (8k or 'original') / hd2880 / hd2160 / hd1440 / hd1080 / hd720 / large / medium / small / tiny
+  /**
+   * 
+   * @param {string} quality - The quality to set the video to. Can be one of the following:
+   * auto / highres (8k or 'original') / hd2880 / hd2160 / hd1440 / hd1080 / hd720 / large / medium / small / tiny
+   * @returns 
+   */
+  var setQuality = (quality) =>
+    document.getElementById("movie_player").setPlaybackQualityRange(quality);
+  var checkElement = () => {
+    element = document.getElementById("movie_player");
+    if (!element) setTimeout(checkElement, 500);
+    else {
+      setQuality("hd1440");
+    }
+  };
+})();

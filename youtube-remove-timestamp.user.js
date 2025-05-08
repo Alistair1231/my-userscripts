d6983c2b0f7d1939d3fdfd893d46eba4634f8af9
Alistair1231 <alistair.bahr@outlook.com>
2025-05-08 12:20:27 +0200

    Update youtube-remove-timestamp.user.js

--- youtube-remove-timestamp.user.js
+++ youtube-remove-timestamp.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Youtube Remove Timestamp
-// @namespace    https://github.com/Auncaughbove17/my-userscripts/
+// @namespace    https://github.com/Alistair1231/my-userscripts/
 // @version      0.1.0
 // @description  Removes the timestamp from URL, so it doesn't invalidate your progress when you reload the page.
 // @author       Alistair1231

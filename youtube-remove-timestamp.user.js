832cb4d65c4dbe7fe3dd1902245af4b8558209e0
Alistair1231 <alistair.bahr@outlook.com>
2025-05-08 12:17:02 +0200

    Update youtube-remove-timestamp.user.js

---
+++ youtube-remove-timestamp.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Youtube Remove Timestamp
+// @namespace    https://github.com/Auncaughbove17/my-userscripts/
+// @version      0.3.3
+// @description  Updates the hi
+// @author       Alistair1231
+// @match        https://mapgenie.io/*
+// @icon         https://icons.duckduckgo.com/ip2/mapgenie.io.ico
+// @license      MIT
+// ==/UserScript==
+// https://greasyfork.org/en/scripts/464497-mapgenie-smaller-icon-size
+// https://openuserjs.org/scripts/Alistair1231/MapGenie_-_Smaller_Icon_Size

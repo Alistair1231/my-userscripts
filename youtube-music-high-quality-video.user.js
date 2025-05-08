f2dce437d2d51e564dcec36c61ac687c6f193cb2
Alistair1231 <alistair.bahr@outlook.com>
2025-05-08 14:42:21 +0200

    Update youtube-music-high-quality-video.user.js

---
+++ youtube-music-high-quality-video.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Youtube Music High Quality Video
+// @namespace    https://github.com/Alistair1231/my-userscripts/
+// @version      0.1.0
+// @description  Sets the video quality to 1440p on Youtube Music
+// @author       Alistair1231
+// @match        https://music.youtube.com/watch*
+// @icon         https://icons.duckduckgo.com/ip2/youtube.com.ico
+// @license      MIT
+// ==/UserScript==
+// https://greasyfork.org/en/scripts/535336-youtube-music-high-quality-video
+// https://github.com/Alistair1231/my-userscripts/blob/master/youtube-music-high-quality-video.user.js
+
+// auto / highres (8k or 'original') / hd2880 / hd2160 / hd1440 / hd1080 / hd720 / large / medium / small / tiny
+var setQuality = (quality) => document.getElementById("movie_player").setPlaybackQualityRange(quality)
+var checkElement = () => {
+	element = document.getElementById("movie_player")
+  if(!element)
+		setTimeout(checkElement,500);
+	else{
+		setQuality("hd1440")
+  }
+}

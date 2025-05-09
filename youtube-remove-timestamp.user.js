4f8a2e4da5c0d7162d30af8f680da56e622c4c3a
Alistair1231 <alistair.bahr@outlook.com>
2025-05-09 08:14:34 +0200

    Update youtube-remove-timestamp.user.js

--- youtube-remove-timestamp.user.js
+++ youtube-remove-timestamp.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Remove Timestamp
 // @namespace    https://github.com/Alistair1231/my-userscripts/
-// @version      0.2.1
+// @version      0.2.2
 // @description  Removes the timestamp from URL, so it doesn't invalidate your progress when you reload the page.
 // @author       Alistair1231
 // @match        https://www.youtube.com/*
@@ -13,16 +13,20 @@
 
 (function () {
   "use strict";
-  // check if on "https://www.youtube.com/watch*", as youtube is a SPA, we need to load the script on the homepage too, but we only want to remove the timestamp on the watch page
-  if (!window.location.href.includes("https://www.youtube.com/watch")) {
-    // if not on the watch page, return
-    return;
-  }
 
   const removeTimestamp = () => {
+    // check if on "https://www.youtube.com/watch*", as youtube is a SPA, we need to load the script on the homepage too, but we only want to remove the timestamp on the watch page
+    if (!window.location.href.includes("https://www.youtube.com/watch")) {
+      // if not on the watch page, return
+      return;
+    }
+
     console.log("Trying to remove timestamp from URL...");
     // Get all the query parameters from the URL, except for the timestamp
-    const search = window.location.search.split("&").filter((x) => !x.startsWith("t=")).join("&");
+    const search = window.location.search
+      .split("&")
+      .filter((x) => !x.startsWith("t="))
+      .join("&");
 
     // Update the URL in the address bar without reloading the page
     window.history.pushState(
@@ -31,8 +35,8 @@
       // build the new URL with the same path and query parameters, but without the timestamp
       `${window.location.origin}${window.location.pathname}${search}`
     );
-  }
-  
+  };
+
   // Wait for 10 seconds before updating the URL. If done too quick, Youtube will re-add the timestamp under certain conditions
   setTimeout(() => {
     removeTimestamp();

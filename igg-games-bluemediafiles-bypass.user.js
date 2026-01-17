9c368f551c56b759250fa7f27895d5d22057ece0
Alistair1231 <alistair1231@gmail.com>
2026-01-17 14:44:00 +0100

    igg

--- igg-games-bluemediafiles-bypass.user.js
+++ igg-games-bluemediafiles-bypass.user.js
@@ -1,60 +1,67 @@
 // ==UserScript==
 // @name         IGG Games / bluemediafiles bypass
 // @namespace    https://github.com/Alistair1231/my-userscripts/
-// @version      1.0.10
+// @version      2.0.0
 // @description  Skip count down and redirect to actual download page.
 // @homepage     https://greasyfork.org/scripts/423435
-// @author       ting / Alistair1231
+// @author       Alistair1231
 // @match        http*://*/*
 // @grant        none
-// @license      MIT
+// @license      GPL3.0
 // ==/UserScript==
-
-/*
-MIT License
-
-Copyright (c) 2022 ting / 2023 Alistair1231
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+// https://github.com/Alistair1231/my-userscripts/blob/master/igg-games-bluemediafiles-bypass.user.js
+// https://greasyfork.org/en/scripts/425739-igg-games-bluemediafiles-auto-continue-after-wait
 
 (function () {
-    "use strict";
-    if (window.location.href.match(/.*bluemedia.*url-generator-?\d+?.php/)) {
-      function _bluemediafiles_decodeKey(encoded) {
-        var key = "";
-        for (var i = encoded.length / 2 - 5; i >= 0; i = i - 2) {
-          key += encoded[i];
-        }
-        for (i = encoded.length / 2 + 4; i < encoded.length; i = i + 2) {
-          key += encoded[i];
-        }
-        return key;
+  // since domain constantly changes, we match all sites, and then filter. 
+  // This is needed for compliance with MV3, since we can't just regex filter
+  // in the script metadata anymore
+  if (/.*bluemedia.*url-generator(-\d+)?\.php/.test(window.location.href)) {
+    let loop;
+
+    function run() {
+      const link = document.querySelector('#btn-download')?.href;
+      if (link && /.*\/get-url.*\.php\?url=.*/.test(link)) {
+        clearInterval(loop);
+        console.log(`Link decoding succeeded, ${link}`);
       }
-      [].forEach.call(document.getElementsByTagName("script"), function (s) {
-        var m = s.innerText.match(
-          /Goroi_n_Create_Button[(]\"(?<encoded>.+?)\"[)];/
-        );
-        if (m && m.length > 1) {
-          window.location = "/get-url.php?url=" + _bluemediafiles_decodeKey(m[1]);
-        }
-      });
     }
-  })();
-  
+
+    function unlock() {
+      try {
+        window.generateDownloadUrl();
+      } catch (e) {
+        console.log("Link decoding failed, retrying");
+      }
+    }
+
+    function unlockAndRun() {
+      unlock();
+      run();
+    }
+
+    setTimeout(() => {
+      loop = setInterval(unlockAndRun, 1000);
+    }, 200);
+
+    //? /////////////////////
+    //? fallback to simple waiting logic if they change the logic again
+    function wait() {
+      // waiting for countown
+      var clickButton;
+      var count = 0;
+      var interval = setInterval(() => {
+        clickButton = document.getElementById("btn-download");
+        count++;
+        console.log("Waited " + count + " seconds");
+        if (clickButton.src !== "") {
+          //clicking
+          clickButton.parentElement.submit();
+          clearInterval(interval);
+        }
+      }, 1000);
+    }
+    setTimeout(wait, 6000);
+
+  }
+})();

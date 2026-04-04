a734f0f735f85a84174615e24a48db748a8523f1
Alistair1231 <alistair1231@gmail.com>
2026-04-04 21:06:54 +0200

    audio ratio

--- html5-audio-ratio.user.js
+++ html5-audio-ratio.user.js
@@ -11,37 +11,45 @@
 // ==/UserScript==
 // https://github.com/Alistair1231/my-userscripts/raw/refs/heads/master/html5-audio-ratio.user.js
 
-(function() {
+(function () {
     'use strict';
 
-    // manipulation exponent, higher value = lower volume
-    // 3 is the value used by pulseaudio, which Barteks2x figured out this gist here: https://gist.github.com/Barteks2x/a4e189a36a10c159bb1644ffca21c02a
-    // 0.05 (or 5%) is the lowest you can select in the UI which with an exponent of 3 becomes 0.000125 or 0.0125%
-    const EXPONENT = 3;
+    const run = () => {
+        // manipulation exponent, higher value = lower volume
+        // 3 is the value used by pulseaudio, which Barteks2x figured out this gist here: https://gist.github.com/Barteks2x/a4e189a36a10c159bb1644ffca21c02a
+        // 0.05 (or 5%) is the lowest you can select in the UI which with an exponent of 3 becomes 0.000125 or 0.0125%
+        const EXPONENT = 3;
 
-    const storedOriginalVolumes = new WeakMap();
-    const {get, set} = Object.getOwnPropertyDescriptor(HTMLMediaElement.prototype, 'volume');
-    Object.defineProperty(HTMLMediaElement.prototype, 'volume', {
-        get () {
-            const lowVolume = get.call(this);
-            const calculatedOriginalVolume = lowVolume ** (1 / EXPONENT);
+        const storedOriginalVolumes = new WeakMap();
+        const { get, set } = Object.getOwnPropertyDescriptor(HTMLMediaElement.prototype, 'volume');
+        Object.defineProperty(HTMLMediaElement.prototype, 'volume', {
+            get() {
+                const lowVolume = get.call(this);
+                const calculatedOriginalVolume = lowVolume ** (1 / EXPONENT);
 
-            // The calculated value has some accuracy issues which can lead to problems for implementations that expect exact values.
-            // To avoid this, I'll store the unmodified volume to return it when read here.
-            // This mostly solves the issue, but the initial read has no stored value and the volume can also change though external influences.
-            // To avoid ill effects, I check if the stored volume is somewhere in the same range as the calculated volume.
-            const storedOriginalVolume = storedOriginalVolumes.get(this);
-            const storedDeviation = Math.abs(storedOriginalVolume - calculatedOriginalVolume);
+                // The calculated value has some accuracy issues which can lead to problems for implementations that expect exact values.
+                // To avoid this, I'll store the unmodified volume to return it when read here.
+                // This mostly solves the issue, but the initial read has no stored value and the volume can also change though external influences.
+                // To avoid ill effects, I check if the stored volume is somewhere in the same range as the calculated volume.
+                const storedOriginalVolume = storedOriginalVolumes.get(this);
+                const storedDeviation = Math.abs(storedOriginalVolume - calculatedOriginalVolume);
 
-            const originalVolume = storedDeviation < 0.01 ? storedOriginalVolume : calculatedOriginalVolume;
-            // console.log('manipulated volume from', lowVolume.toFixed(2), 'to  ', originalVolume.toFixed(2), storedDeviation);
-            return originalVolume;
-        },
-        set (originalVolume) {
-            const lowVolume = originalVolume ** EXPONENT;
-            storedOriginalVolumes.set(this, originalVolume);
-            // console.log('manipulated volume to  ', lowVolume.toFixed(2), 'from', originalVolume.toFixed(2));
-            set.call(this, lowVolume);
-        }
-    });
+                const originalVolume = storedDeviation < 0.01 ? storedOriginalVolume : calculatedOriginalVolume;
+                // console.log('manipulated volume from', lowVolume.toFixed(2), 'to  ', originalVolume.toFixed(2), storedDeviation);
+                return originalVolume;
+            },
+            set(originalVolume) {
+                const lowVolume = originalVolume ** EXPONENT;
+                storedOriginalVolumes.set(this, originalVolume);
+                // console.log('manipulated volume to  ', lowVolume.toFixed(2), 'from', originalVolume.toFixed(2));
+                set.call(this, lowVolume);
+            }
+        });
+    };
+
+    // if url match spotify, don't run
+    if (window.location.href.includes('spotify.com')) {
+        return;
+    }
+    run();
 })();
